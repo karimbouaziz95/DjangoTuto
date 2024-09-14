b46b07ce7dce57b04bb579f3e3c47dad819d5f0c
karimbouaziz95 <karimbouazizpip@gmail.com>
2024-09-14 22:34:16 +0200

    New project monthly challenges + url&views + redirect

---
+++ monthly_challenges/challenges/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+monthly_challenges = {
+    "january": "Jenya Korkeshko",
+    "february": "Sbou3i",
+    "march": "Tarak Bouaziz",
+    "april": "Chayma Bouaziz",
+    "may": "Looouzen",
+    "june": "Mee & Hamdi",
+    "july": "Kush",
+    "august": "Don't know aout",
+    "september": "Nejah ben Rhaiem",
+    "october": "October too",
+    "november": "Also novemeber",
+    "december": "Mom & Dad <3"
+}
+
+months_by_number = {
+    1: "january",
+    2: "february",
+    3: "march",
+    4: "april",
+    5: "may",
+    6: "june",
+    7: "july",
+    8: "august",
+    9: "september",
+    10: "october",
+    11:"november",
+    12: "december"
+}
+
+# Create your views here.
+
+
+def monthly_challenge(request, month):
+    try:
+        response_text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound("Not supported")
+    return HttpResponse(response_text)
+
+def monthly_challenge_by_number(request, month):
+    try:
+        return HttpResponseRedirect("/challenges/" + months_by_number[month])   # Redirect to another url
+    except:
+        return HttpResponse(f"Your lucky number is: {month}")
